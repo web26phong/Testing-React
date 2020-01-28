935070b033d96001a971e7edda46fd1ec8685968
Phong Yang <phongyang88@hotmail.com>
2020-01-27 20:00:43 -0600

    it works. not 100% sure why

--- star-wars-app/src/app.test.js
+++ star-wars-app/src/app.test.js
@@ -1,32 +1,29 @@
 import React from "react";
 import {render, fireEvent, wait} from "@testing-library/react";
-import App from "../App";
-import StarWarsCharacters from "./StarWarsCharacters";
-import {getData as mockGetData} from "../api";
+import StarWarsCharacters from "./components/StarWarsCharacters";
+import {getData as mockGetData} from "./api";
 
-jest.mock('../api/');
-
-const initialData = {
-    next: "next link",
-    previous: null,
-    results: [
-        {name: "name1", url: "asdf"}, 
-        {name: "name2", url: "asdf"}
-    ]
-}
+jest.mock('./api/');
 
 test("Buttons rendered and working", async ()=>{
+    mockGetData.mockResolvedValue({
+        next: "next link",
+        previous: "previous link",
+        results: [
+            {name: "name1", url: "url1"}, 
+            {name: "name2", url: "url2"}
+        ]
+    })
+
     const {getByText} = render(<StarWarsCharacters />);
     const previousButton = getByText(/previous/i);
     const nextButton = getByText(/next/i);
 
-    mockGetData.mockResolvedValue(initialData)
-
-
+    
     // const url = initialData.next;
 
     fireEvent.click(previousButton);
-    // fireEvent.click(nextButton);
+    fireEvent.click(nextButton);
     expect(mockGetData).toHaveBeenCalledTimes(1);
     expect(mockGetData).toHaveBeenCalledWith("https://swapi.co/api/people");
 
